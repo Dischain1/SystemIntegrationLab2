575efef1d19f8f1d611c159cb2183b1148229485
Dischain1 <Dischain123321@gmail.com>
2019-12-11 15:06:03 +0700

    crud in data service

--- SystemIntegrationLab2/DataService.svc.cs
+++ SystemIntegrationLab2/DataService.svc.cs
@@ -1,6 +1,7 @@
 ﻿using Model;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 
 namespace SystemIntegrationLab2
@@ -12,6 +13,11 @@ namespace SystemIntegrationLab2
         public DataService()
         {
             db = new Context();
+        }
+
+        public string Test()
+        {
+            return "hello";
         }
 
         #region ReadOperastions
@@ -34,7 +40,6 @@ namespace SystemIntegrationLab2
                 return new RespondAndDataList<DepositDto> { Respond = Respond.CreateErrorRespond(e.Message)};
             }
         }
-
         public RespondAndDataList<WellDto> ReadWells(Guid depositId)
         {
             try
@@ -56,7 +61,6 @@ namespace SystemIntegrationLab2
                 return new RespondAndDataList<WellDto> { Respond = Respond.CreateErrorRespond(e.Message)};
             }
         }
-
         public RespondAndDataList<ExtractionDto> ReadExtractions(Guid wellId, string dateFrom, string dateTo)
         {
             try
@@ -83,17 +87,194 @@ namespace SystemIntegrationLab2
                 return new RespondAndDataList<ExtractionDto> { Respond = Respond.CreateErrorRespond(e.Message) };
             }
         }
+        #endregion
 
-        public string Test()
+        #region DeleteOperastions
+        public Respond DeleteDeposit(Guid depositId)
         {
-            return "hello";
+            try
+            {
+                Месторождения deposit = db.Месторождения.Find(depositId);
+
+                if (!deposit.Скважины.Any())
+                {
+                    db.Месторождения.Remove(deposit);
+                    db.SaveChanges();
+                    return Respond.CreateOkRespond();
+                }
+                else
+                {
+                    return Respond.CreateErrorRespond("Сначала нужно удалить связанные Скважины.");
+                }
+
+            }
+            catch (Exception e)
+            {
+                return Respond.CreateErrorRespond(e.Message);
+            }
         }
-
-        public List<DepositDto> GetDeposits()
+        public Respond DeleteWell(Guid wellId)
         {
-            return db.Месторождения
-                .Select(x => new DepositDto { ID = x.ID, Name = x.Название })
-                .ToList();
+            try
+            {
+                Скважины well = db.Скважины.Find(wellId);
+                if (!well.Добыча.Any())
+                {
+                    db.Скважины.Remove(well);
+                    db.SaveChanges();
+
+                    return Respond.CreateOkRespond();
+                }
+                else
+                {
+                    return Respond.CreateErrorRespond("Сначала нужно удалить связанные Добычи.");
+                }
+            }
+            catch (Exception e)
+            {
+                return Respond.CreateErrorRespond(e.Message);
+            }
+        }
+        public Respond DeleteExtraction(Guid extractionId)
+        {
+            try
+            {
+                Добыча extraction = db.Добыча.Find(extractionId);
+
+                db.Добыча.Remove(extraction);
+                db.SaveChanges();
+
+                return Respond.CreateOkRespond();
+            }
+            catch (Exception e)
+            {
+                return Respond.CreateErrorRespond(e.Message);
+            }
+        }
+        #endregion
+
+        #region UpdateOperastions
+        public Respond UpdateDeposit(Guid depositId, string name)
+        {
+            try
+            {
+                Месторождения deposit = db.Месторождения.Find(depositId);
+                deposit.Название = name;
+                //db.Entry(deposit).State = EntityState.Modified;
+                db.SaveChangesAsync();
+                return Respond.CreateOkRespond();
+            }
+            catch (Exception e)
+            {
+                return Respond.CreateErrorRespond(e.Message);
+            }
+        }
+        public Respond UpdateWell(Guid wellId, string number)
+        {
+            try
+            {
+                Скважины well = db.Скважины.Find(wellId);
+                well.Номер = number;
+                db.Entry(well).State = EntityState.Modified;
+                db.SaveChangesAsync();
+
+                return Respond.CreateOkRespond();
+
+            }
+            catch (Exception e)
+            {
+                return Respond.CreateErrorRespond(e.Message);
+            }
+        }
+        public Respond UpdateExtraction(Guid extractionId, string date, int value)
+        {
+            try
+            {
+                Добыча extraction = db.Добыча.Find(extractionId);
+                extraction.Дата = date.GetDate();
+                extraction.Значение = value;
+
+                db.Entry(extraction).State = EntityState.Modified;
+                db.SaveChangesAsync();
+
+                return Respond.CreateOkRespond();
+
+            }
+            catch (Exception e)
+            {
+                return Respond.CreateErrorRespond(e.Message);
+            }
+        }
+        #endregion
+
+        #region CreateOperastions
+        public Respond CreateDeposit(string name, string type, string openDate)
+        {
+            try
+            {
+                Месторождения deposit = new Месторождения
+                {
+                    Название = name,
+                    Тип = type,
+                    Дата_открытия = openDate.GetDate()
+                };
+
+                db.Месторождения.Add(deposit);
+                db.SaveChangesAsync();
+
+                return Respond.CreateOkRespond();
+
+            }
+            catch (Exception e)
+            {
+                return Respond.CreateErrorRespond(e.Message);
+            }
+        }
+        public Respond CreateWell(Guid depositId, string number, string type, int depth, string drillingDate)
+        {
+            try
+            {
+                Скважины well = new Скважины
+                {
+                    Номер = number,
+                    Глубина = depth,
+                    Тип = type,
+                    Дата_бурения = drillingDate.GetDate(),
+                    ID_Месторождения = depositId
+                };
+
+                db.Скважины.Add(well);
+
+                db.SaveChangesAsync();
+                return Respond.CreateOkRespond();
+
+            }
+            catch (Exception e)
+            {
+                return Respond.CreateErrorRespond(e.Message);
+            }
+        }
+        public Respond CreateExtraction(Guid wellId, string date, int value)
+        {
+            try
+            {
+                Добыча extraction = new Добыча
+                {
+                    Дата = date.GetDate(),
+                    Значение = value,
+                    ID_Скважины = wellId
+                };
+
+                db.Добыча.Add(extraction);
+
+                db.SaveChangesAsync();
+                return Respond.CreateOkRespond();
+
+            }
+            catch (Exception e)
+            {
+                return Respond.CreateErrorRespond(e.Message);
+            }
         }
         #endregion
     }
--- SystemIntegrationLab2/IDataService.cs
+++ SystemIntegrationLab2/IDataService.cs
@@ -11,14 +11,36 @@ namespace SystemIntegrationLab2
         [OperationContract]
         string Test();
 
+        // create
         [OperationContract]
-        List<DepositDto> GetDeposits();
+        Respond CreateDeposit(string name, string type, string openDate);
+        [OperationContract]
+        Respond CreateWell(Guid depositId, string number, string type, int depth, string drillingDate);
+        [OperationContract]
+        Respond CreateExtraction(Guid wellId, string date, int value);
 
+        // read
         [OperationContract]
         RespondAndDataList<DepositDto> ReadDeposits();
         [OperationContract]
         RespondAndDataList<WellDto> ReadWells(Guid depositId);
         [OperationContract]
         RespondAndDataList<ExtractionDto> ReadExtractions(Guid wellId, string dateFrom, string dateTo);
+        
+        // update
+        [OperationContract]
+        Respond UpdateDeposit(Guid depositId, string name);
+        [OperationContract]
+        Respond UpdateWell(Guid wellId, string number);
+        [OperationContract]
+        Respond UpdateExtraction(Guid extractionId, string date, int value);
+
+        // delete
+        [OperationContract]
+        Respond DeleteDeposit(Guid depositId);
+        [OperationContract]
+        Respond DeleteWell(Guid wellId);
+        [OperationContract]
+        Respond DeleteExtraction(Guid extractionId);
     }
 }
